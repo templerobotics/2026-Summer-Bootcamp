826e81745cccfb2209571e1efb713658c64c6ffc
HecatesRevenge <87406020+HecatesRevenge@users.noreply.github.com>
2026-06-04 16:00:40 -0400

    Added unit conversion C++
    Added file to convert python unit conversion to C++
    Outputs menu

---
+++ Programming Practice/Amelia/unit_convert.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+float convertUnits(int input) {
+  cout
+      << ("Select conversion type:\n1. Meters to Feet\n2. Inches to "
+          "Centimeters\n3.Celsius to Fahrenheit\n");
+  return 0;
+};
+
+int main() {
+  convertUnits(1);
+  return 0;
+}
